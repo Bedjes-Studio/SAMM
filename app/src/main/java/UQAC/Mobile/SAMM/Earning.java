91f27e6e7ddc85b509e5d1a4d126e6b33af00692
Hugo Langlais <bedjestudio@gmail.com>
2022-11-27 22:23:49 -0500

    fix event exemple

--- app/src/main/java/UQAC/Mobile/SAMM/Earning.java
+++ app/src/main/java/UQAC/Mobile/SAMM/Earning.java
@@ -1,11 +1,15 @@
 package UQAC.Mobile.SAMM;
 
+import java.util.Date;
+
 public class Earning extends Event {
     private float value;
     private String reason;
 
-    public Earning(float value, String reason) {
+    public Earning(float value, String reason, Date date, int mileage) {
         this.value = value;
         this.reason = reason;
+        this.date = date;
+        this.mileage = mileage;
     }
 }
