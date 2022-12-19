074de61dea922410daf76b3e2b424ec2a6ccd49d
LilianMgn <lilianmgn1@gmail.com>
2022-12-19 00:08:33 -0500

    stats

---
+++ app/src/main/java/UQAC/Mobile/SAMM/Statistiques.java
@@ -0,0 +1,14 @@
+package UQAC.Mobile.SAMM;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Statistiques extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
