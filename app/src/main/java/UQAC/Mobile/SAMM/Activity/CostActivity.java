9d229750375e98db29159ffb54ef36fbc5800315
Hugo Langlais <bedjestudio@gmail.com>
2023-01-01 23:11:02 -0500

    Clean code

--- app/src/main/java/UQAC/Mobile/SAMM/Activity/CostActivity.java
+++ app/src/main/java/UQAC/Mobile/SAMM/Activity/CostActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 - Hugo LANGLAIS & Alexia LACOMBE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package UQAC.Mobile.SAMM.Activity;
 
 import android.app.DatePickerDialog;
@@ -27,61 +43,68 @@ import UQAC.Mobile.SAMM.R;
 
 public class CostActivity extends AppCompatActivity {
 
-    final Calendar myCalendar = Calendar.getInstance();
+    private final Calendar myCalendar = Calendar.getInstance();
 
-    FloatingActionButton back;
-    Button save;
+    private FloatingActionButton back;
+    private Button save;
 
-    EditText value;
-    EditText reason;
-    EditText mileage;
-    EditText dateText;
+    private EditText value;
+    private EditText reason;
+    private EditText mileage;
+    private EditText dateText;
+
+    private String intentId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cost);
 
+        readIntendExtras();
+        findViewInLayout();
+
+        setOnClickListeners();
+    }
+
+    private void readIntendExtras() {
+        Intent intent = getIntent();
+        intentId = intent.getExtras().getString("id");
+    }
+
+    private void findViewInLayout() {
         value = findViewById(R.id.valueText);
         reason = findViewById(R.id.reasonText);
         mileage = findViewById(R.id.mileageText);
-
-        Intent intent = getIntent();
-        String id = intent.getExtras().getString("id");
-        Log.d("ALEXIA", id);
-
-        NetworkManager networkManager = new NetworkManager();
-
-        //-- Date Picker
+        save = findViewById(R.id.button_save_event);
+        back = findViewById(R.id.backButtonCreationV);
         dateText = (EditText) findViewById(R.id.dateText);
-
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         dateText.setText(dateFormat.format(new Date())); // it will show 16/07/2013
+    }
 
-        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
+    private void setOnClickListeners() {
+
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int day) {
                 myCalendar.set(Calendar.YEAR, year);
-                myCalendar.set(Calendar.MONTH,month);
-                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, day);
                 updateLabel();
             }
         };
+
         dateText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new DatePickerDialog(CostActivity.this,date,
+                new DatePickerDialog(CostActivity.this, date,
                         myCalendar.get(Calendar.YEAR),
                         myCalendar.get(Calendar.MONTH),
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
 
-        // --Save
-        save = findViewById(R.id.button_save_event);
-
-        back = findViewById(R.id.backButtonCreationV);
-
+        // TODO : clean this listener
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -89,18 +112,18 @@ public class CostActivity extends AppCompatActivity {
                 String reasonValue = reason.getText().toString();
                 Integer mileageValue = Integer.valueOf(mileage.getText().toString());
 
-                Earning earning = new Earning( reasonValue, valueValue, myCalendar.getTime(),  mileageValue);
+                Earning earning = new Earning(reasonValue, valueValue, myCalendar.getTime(), mileageValue);
 
                 earning.save(earning);
 
 //                Toast.makeText(getApplicationContext(),"save", Toast.LENGTH_SHORT).show();
-                if(true){//!nom.getText().toString().isEmpty() && !marque.getText().toString().isEmpty() && !modele.getText().toString().isEmpty() && !numImmat.getText().toString().isEmpty() && !typeCarbu.getText().toString().isEmpty() && !capacite.getText().toString().isEmpty() && !kilometrage.getText().toString().isEmpty()){
+                if (true) {//!nom.getText().toString().isEmpty() && !marque.getText().toString().isEmpty() && !modele.getText().toString().isEmpty() && !numImmat.getText().toString().isEmpty() && !typeCarbu.getText().toString().isEmpty() && !capacite.getText().toString().isEmpty() && !kilometrage.getText().toString().isEmpty()){
                     //Creer nouveau véhicule ici pour la bdd
                     //Car vehicule = new Car(new History(), null /*pour le moment je met nul mais à changer*/, Integer.parseInt(kilometrage.getText().toString()), typeCarbu.getText().toString(), Integer.parseInt(capacite.getText().toString()), spinner.getAdapter().toString(), marque.getText().toString(), modele.getText().toString(), nom.getText().toString());
-                    networkManager.events.add(earning);
+                    NetworkManager.events.add(earning);
                     Intent returnMenuIntent = new Intent(CostActivity.this, EventActivity.class);
                     startActivity(returnMenuIntent);
-                }else{
+                } else {
                     Toast.makeText(CostActivity.this, "Champ manquant ou mal complété !", Toast.LENGTH_SHORT).show();
                 }
 
@@ -113,15 +136,15 @@ public class CostActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent returnMenuIntent = new Intent(CostActivity.this, EventActivity.class);
-                returnMenuIntent.putExtra("id", id);
+                returnMenuIntent.putExtra("id", intentId);
                 startActivity(returnMenuIntent);
             }
         });
     }
 
-    private void updateLabel(){
-        String myFormat="MM/dd/yy";
-        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
         dateText.setText(dateFormat.format(myCalendar.getTime()));
     }
 
