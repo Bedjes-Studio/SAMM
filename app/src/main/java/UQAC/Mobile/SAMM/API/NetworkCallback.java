df8112bde49b08b5fb6dfa6e016d3ce2ee227ebe
Hugo Langlais <bedjestudio@gmail.com>
2023-01-10 16:31:34 -0500

    Add licence

--- app/src/main/java/UQAC/Mobile/SAMM/API/NetworkCallback.java
+++ app/src/main/java/UQAC/Mobile/SAMM/API/NetworkCallback.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 - Hugo LANGLAIS & Alexia LACOMBE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package UQAC.Mobile.SAMM.API;
 
 import java.util.List;
@@ -8,6 +24,9 @@ import UQAC.Mobile.SAMM.Base.Earning;
 import UQAC.Mobile.SAMM.Base.Event;
 import UQAC.Mobile.SAMM.Base.Refuel;
 
+/**
+ * NetworkCallbacks are calls after a request when
+ */
 public class NetworkCallback {
     public void onActionSuccess(){};
     public void onActionSuccess(Car[] cars){};
